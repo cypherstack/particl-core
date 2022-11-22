8962b97e78b49ed9dc7d949de831f1428c097beb
likho <likhojiba@gmail.com>
2022-11-22 12:32:37 +0200

    ADd missing constants

--- src/secp256k1/src/ecmult_gen.h
+++ src/secp256k1/src/ecmult_gen.h
@@ -10,6 +10,13 @@
 #include "scalar.h"
 #include "group.h"
 
+#ifndef ECMULT_GEN_PREC_BITS
+#  define ECMULT_GEN_PREC_BITS 4
+#  ifdef DEBUG_CONFIG
+#     pragma message DEBUG_CONFIG_MSG("ECMULT_GEN_PREC_BITS undefined, assuming default value")
+#  endif
+#endif
+
 #if ECMULT_GEN_PREC_BITS != 2 && ECMULT_GEN_PREC_BITS != 4 && ECMULT_GEN_PREC_BITS != 8
 #  error "Set ECMULT_GEN_PREC_BITS to 2, 4 or 8."
 #endif
